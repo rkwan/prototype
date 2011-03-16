d09123e370d210468cb37304f2a6cb608e4fd452
Ricky Kwan <xzeitgeber@gmail.com>
2011-03-15 17:52:22 -0700

    added main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#news_label').click(function() {
+		$('#tab_selected').prependTo("news_tab");
+	});
+	$('#news_label').click(function() {
+		$('#tab_selected').prependTo("friends_tab");
+	});
+});
